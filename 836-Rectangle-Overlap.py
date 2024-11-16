4d7ad71057f8de6695e160c072bb81e34aba7d02
santhoshbethi <45058712+santhoshbethi@users.noreply.github.com>
2024-11-16 09:49:59 -0500

    [836] [Time Beats: 100.00%] [Memory Beats: 15.98%] - LeetPush

---
+++ 836-Rectangle-Overlap.py
@@ -0,0 +1,3 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        return (rec1[2]>rec2[0] and rec1[3]>rec2[1]) and rec2[2]>rec1[0] and rec2[3]>rec1[1]
