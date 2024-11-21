44e172cb5f819681ae93094006df7726edb07fd1
santhoshbethi <45058712+santhoshbethi@users.noreply.github.com>
2024-11-20 20:47:09 -0500

    [415] [Time Beats: 10.29%] [Memory Beats: 8.21%] - LeetPush

--- 415-Add-Strings.py
+++ 415-Add-Strings.py
@@ -2,23 +2,30 @@ class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         num2=num2[::-1]
         num1=num1[::-1]
-        res_length = max(len(num1), len(num2))
-        num1 = num1.ljust(res_length, '0')
-        num2 = num2.ljust(res_length, '0')
+        res_length=len(num2) if len(num2)>len(num1) else len(num1)
+        if len(num1)<res_length:
+            append_len=res_length-len(num1)
+            for _ in range(append_len):
+                num1=num1+\0\
+        if len(num2)<res_length:
+            append_len=res_length-len(num2)
+            for _ in range(append_len):
+                num2=num2+\0\
         res=[]
         i=0
-        temp=0
         carry=0
         while res_length>i:
             temp=int(num1[i])+int(num2[i])+carry
 
             carry=temp//10
             temp=temp%10
+            print(temp)
             res.append(str(temp))
             i+=1
+        
         if carry:
             res.append(str(carry))
-        return \\.join(res[::-1])
+        return  \\.join(res[::-1])
                 
             
 
